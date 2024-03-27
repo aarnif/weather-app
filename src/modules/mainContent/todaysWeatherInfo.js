226429431cf3ae4b370245c005b6c75c9e953858
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-03-27 09:51:33 +0200

    Add styling to todays weather info

--- src/modules/mainContent/todaysWeatherInfo.js
+++ src/modules/mainContent/todaysWeatherInfo.js
@@ -2,8 +2,15 @@ import utils from "../../utils/utils.js";
 
 const displayTodaysWeatherInfo = (displayUnit, todaysWeatherInfo) => {
   const percent = "%";
-  const weatherInfo = document.createElement("div");
+  const weatherInfoContainer = document.createElement("div");
+  weatherInfoContainer.className = "flex justify-center items-center";
+
+  const weatherInfoContent = document.createElement("div");
+  weatherInfoContent.className =
+    "flex-grow max-w-[1000px] flex justify-center items-center";
+
   const weatherInfoList = document.createElement("ul");
+  weatherInfoList.className = "w-full";
 
   const feelDegree =
     displayUnit === "Celcius"
@@ -32,11 +39,14 @@ const displayTodaysWeatherInfo = (displayUnit, todaysWeatherInfo) => {
   ];
 
   for (let i = 0; i < weatherInfoListItems.length; i += 2) {
-    const listItem = document.createElement("li");
+    const weatherInfoRow = document.createElement("li");
+    weatherInfoRow.className = "weather-info-row";
     const rowItems = weatherInfoListItems.slice(i, i + 2);
 
     for (let j = 0; j < 2; ++j) {
-      const subList = document.createElement("ul");
+      const weatherInfoRowItem = document.createElement("ul");
+      weatherInfoRowItem.className =
+        "w-full flex flex-col justify-center items-center";
       const listItemHeader = document.createElement("li");
       const listItemValue = document.createElement("li");
       const objectKey = Object.keys(rowItems[j])[0];
@@ -50,15 +60,17 @@ const displayTodaysWeatherInfo = (displayUnit, todaysWeatherInfo) => {
         listItemValue.textContent = objectValue;
       }
 
-      subList.appendChild(listItemHeader);
-      subList.appendChild(listItemValue);
-      listItem.appendChild(subList);
-      weatherInfoList.appendChild(listItem);
+      weatherInfoRowItem.appendChild(listItemHeader);
+      weatherInfoRowItem.appendChild(listItemValue);
+      weatherInfoRow.appendChild(weatherInfoRowItem);
+      weatherInfoList.appendChild(weatherInfoRow);
     }
   }
 
-  weatherInfo.appendChild(weatherInfoList);
-  return weatherInfo;
+  weatherInfoContent.appendChild(weatherInfoList);
+
+  weatherInfoContainer.appendChild(weatherInfoContent);
+  return weatherInfoContainer;
 };
 
 export default displayTodaysWeatherInfo;
